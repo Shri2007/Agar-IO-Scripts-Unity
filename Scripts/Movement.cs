5a3a1d8f771f2683e10245650f0bce0c1cc5ddfb
Shri2007 <66092664+Shri2007@users.noreply.github.com>
2020-12-13 16:43:14 -0800

    Add files via upload

---
+++ Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Target.z  = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, moveSpeed * Time.deltaTime / transform.localScale.x);
+    }
+}
